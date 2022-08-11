1295b8b80c2348a5627b9b542d3f9414568a963a
Daniel Galindo <josedaniel.galindo@hotmail.com>
2022-08-11 12:15:23 -0500

    Modify Calculator.js with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,57 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import '../style/calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.refreshOutput = this.refreshOutput.bind(this);
-  }
-
-  refreshOutput = (operator) => {
-    this.setState((prevState) => (calculate(prevState, operator)));
+const Calculator = () => {
+  const [calcOutput, establisCalc] = useState({ total: null, next: null, operation: null });
+  const refreshOutput = (operator) => {
+    establisCalc((stablish) => calculate(stablish, operator));
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    const nullState = () => !total && !next && !operation;
-    const result = () => {
-      if (operation) return `${total} ${operation} ${next || ''}`;
-      return next || total;
-    };
-
-    return (
-      <div className="container">
-        <div className="input">{nullState() ? '0' : result()}</div>
-        <div className="calculator">
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('AC'); }}>AC</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('+/-'); }}>+/-</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('%'); }}>%</button>
-          <button className="calc-button-operator" type="button" onClick={() => { this.refreshOutput('÷'); }}>÷</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('7'); }}>7</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('8'); }}>8</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('9'); }}>9</button>
-          <button className="calc-button-operator" type="button" onClick={() => { this.refreshOutput('x'); }}>x</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('4'); }}>4</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('5'); }}>5</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('6'); }}>6</button>
-          <button className="calc-button-operator" type="button" onClick={() => { this.refreshOutput('-'); }}>-</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('1'); }}>1</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('2'); }}>2</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('3'); }}>3</button>
-          <button className="calc-button-operator" type="button" onClick={() => { this.refreshOutput('+'); }}>+</button>
-          <button className="calc-button-zero" type="button" onClick={() => { this.refreshOutput('0'); }}>0</button>
-          <button className="calc-button" type="button" onClick={() => { this.refreshOutput('.'); }}>.</button>
-          <button className="calc-button-operator" type="button" onClick={() => { this.refreshOutput('='); }}>=</button>
-        </div>
+  const { total, next, operation } = calcOutput;
+  const nullState = () => !total && !next && !operation;
+  const result = () => {
+    if (operation) return `${total} ${operation} ${next || ''}`;
+    return next || total;
+  };
+  return (
+    <div className="container">
+      <div className="input">{nullState() ? '0' : result()}</div>
+      <div className="calculator">
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('AC'); }}>AC</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('+/-'); }}>+/-</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('%'); }}>%</button>
+        <button className="calc-button-operator" type="button" onClick={() => { refreshOutput('÷'); }}>÷</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('7'); }}>7</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('8'); }}>8</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('9'); }}>9</button>
+        <button className="calc-button-operator" type="button" onClick={() => { refreshOutput('x'); }}>x</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('4'); }}>4</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('5'); }}>5</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('6'); }}>6</button>
+        <button className="calc-button-operator" type="button" onClick={() => { refreshOutput('-'); }}>-</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('1'); }}>1</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('2'); }}>2</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('3'); }}>3</button>
+        <button className="calc-button-operator" type="button" onClick={() => { refreshOutput('+'); }}>+</button>
+        <button className="calc-button-zero" type="button" onClick={() => { refreshOutput('0'); }}>0</button>
+        <button className="calc-button" type="button" onClick={() => { refreshOutput('.'); }}>.</button>
+        <button className="calc-button-operator" type="button" onClick={() => { refreshOutput('='); }}>=</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
